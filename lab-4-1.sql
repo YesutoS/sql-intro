18c8aecaba118bd19dfe83782fdb029b947cf1cb
YesutoS <yesuto.shaw@kellogg.northwestern.edu>
2022-01-13 03:49:48 +0000

    A few SQL commands

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,15 @@
 -- 2935
 
 
+select hits, count(*) from stats where player = "Barry Bonds";
+
+--My solution (Correct)
+select stats.id, players.first_name, players.last_name, sum(stats.hits)
+from stats inner join players on stats.player_id = players.id
+where players.first_name = "Barry" and players.last_name = "Bonds";
+
+--His solution (Correct)
+select sum(stats.hits)
+from stats inner join players on players.id = stats.player_id
+where first_name = "Barry"
+and last_name = "Bonds";
--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,12 @@
 -- Drop the students table
 
 -- Recreate it
+
+DROP TABLE students;
+
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
+);
+
